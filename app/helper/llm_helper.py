cdbe6967ae41da09d49cc3e3f1269de5976b68d1
Satyam kumar <sk6005848@gmail.com>
2025-02-10 13:58:37 +0530

    Update llm_helper.py

--- app/helper/llm_helper.py
+++ app/helper/llm_helper.py
@@ -1,22 +1,25 @@
 import os
 from crewai import LLM
+import toml
+
+config = toml.load("secrets.toml")
+
 
 class LlmHelper:
 
     # Define the LLM connection
     def llamaConnection():
         llm = LLM(
-            base_url=os.getenv("LLAMA_BASE_URL"),
-            model=os.getenv("LLAMA_MODEL")
+            base_url=config["LLAMA_BASE_URL"],
+            model=config["LLAMA_MODEL"]
         )
         return llm
     
     # Define the Gemini connection
     def GeminiConnection():
         llm = LLM(
-            model=os.getenv("GEMINI_MODEL"),
-            api_key=os.getenv("GEMINI_API_KEY"),
-            # base_url="https://generativelanguage.googleapis.com"
+            model=config["GEMINI_MODEL"],
+            api_key=config["GEMINI_API_KEY"],
         )
         return llm
 
